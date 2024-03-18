811d74b4a3ac3d02129785776e0e949428a7d1f6
Josh <160798171+Joshh4@users.noreply.github.com>
2024-03-19 10:46:39 +1100

    Add form support into v3

--- project-1/web_server_3.py
+++ project-1/web_server_3.py
@@ -8,27 +8,65 @@ class HTML_REQUEST:
     type: str
     filename: str
     protocol: str
-    kwargs: typing.Dict[str, str]
+    header_kwargs: typing.Dict[str, str]
+
+@dataclasses.dataclass
+class HTML_FORM_POST:
+    type: str
+    filename: str
+    protocol: str
+    header_kwargs: typing.Dict[str, str]
+    form_values: typing.Dict[str, str]
 
 def parse_request(request:bytes) -> HTML_REQUEST:
     request = request.decode()
-    lines = request.split("\r\n")
+    parts = request.split("\r\n\r\n")
 
-    request_type, filename, protocol = lines[0].split(" ", 2)
+    header = parts[0]
+    body = None
+    if len(parts) > 1 and len(parts[1]) > 0:
+        body = parts[1]
 
-    kwargs = {}
-    for line in lines[1:]:
+    header_lines = header.split("\r\n")
+
+    request_type, filename, protocol = header_lines[0].split(" ", 2)
+
+    header_kwargs = {}
+    for line in header_lines[1:]:
         if len(line) == 0:
             break
 
         key, value = line.split(": ", 1)
-        kwargs[key] = value
+        header_kwargs[key] = value
+    
+    body_content = None
+    if body is not None:
+
+        body_lines = body.split("\r\n")
+
+        if header_kwargs.get("Content-Type") == "application/x-www-form-urlencoded":
+            form_data = body_lines[0]
+
+            # form_values = {}
+            # # for entry in form_data.split("&"):
+            # #     k, v = entry.split("=",1)
+            # #     form_values[k] = v
+
+            form_values = {k:v for k,v in [e.split("=",1) for e in form_data.split("&")]}
+
+            return HTML_FORM_POST(
+                type=request_type,
+                filename=filename,
+                protocol=protocol,
+                header_kwargs=header_kwargs,
+                form_values=form_values
+            )
     
     return HTML_REQUEST(
         type=request_type,
         filename=filename,
         protocol=protocol,
-        kwargs=kwargs)
+        header_kwargs=header_kwargs)
 
 
 
@@ -54,7 +92,9 @@ while True:
         # Write a parser that gathers args from headers and things
         req = parse_request(message)
 
-        print(req)
+        if isinstance(req, HTML_FORM_POST):
+            print("Form entries recv")
+            print(req.form_values)
 
         filename = message.split()[1].decode()
         with open(filename[1:], "r") as buffer:
@@ -75,7 +115,7 @@ while True:
         conn.close()
 
     except IOError:
-        print(f"IOError, assuming 404")
+        print(f"IOError, assuming 404 - {filename}")
 
         # Send response message for file not found
         conn.send("HTTP/1.1 200 OK\r\n".encode())
@@ -85,6 +125,6 @@ while True:
         # Close client socket
         conn.close()
 
-        # Close the server socket
-        server_sock.close()
-        sys.exit() # Terminate the program after sending the corresponding data
+# Close the server socket
+server_sock.close()
+sys.exit() # Terminate the program after sending the corresponding data
