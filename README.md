31aa33826232270fa62f33c4551927324c1091a1
Josh <challo4joshdj@gmail.com>
2024-03-30 22:32:21 +1100

    Reformat and reorganize repository

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# networking-fundamentals
+# 1. Table Of Contents
+- [1. Table Of Contents](#1-table-of-contents)
+- [2. Networking Fundamentals (41092)](#2-networking-fundamentals-41092)
+
+# 2. Networking Fundamentals (41092)
+ This repository is for storing lab work and assessment parts for my
+ Network Fundamentals University class (NetFun, course# 41092).
+
+ The code in this repository is in the process of being cleaned up
+ and rearranged. If something looks messy feel free to let me know
+ by opening an issue in the issue tracker. Thanks!
--- web-server-experiments/webserver/wserve_test.py
+++ web-server-experiments/webserver/wserve_test.py
@@ -1,3 +1,16 @@
+"""wserve_test.py
+
+This file serves as a testing grounds for WServe,
+a web server experiment I created.
+
+WServe's syntax and use of decorators was based on
+that of Flask's.
+
+wserve_test.py must be run from the working directory
+/web-server-experiments/webserver/ or else it will
+not run properly.
+"""
+
 import wserve
 
 ws = wserve.WebServer()
@@ -14,5 +27,6 @@ def root(request=None):
 def root(request=None):
     return 200, "<p>URL arguments are displayed here. Add arguments to the url with &k=v</p><ul>"+"".join([f"<li>{k} = {v}</li>" for k,v in request.args.items()])+"<ul>"
 
+# Run web server on port 3882.
+# Connect on a browser with http://localhost:3882/
 ws.run("", 3882)
-
